793133a81c0d462e2bd67f6124ae9e56d789f546
mavarius <metalwhirlwind@yahoo.com>
2016-10-31 12:13:45 -0700

    final commit

--- server/models/Client.js
+++ server/models/Client.js
@@ -1,60 +1,44 @@
 const mongoose = require('mongoose')
+const moment = require('moment')
 
 const clientSchema = new mongoose.Schema({
   name: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true }
+    firstName: { type: String, minlength: [1, 'must enter first name'] },
+    lastName: { type: String, minlength: [1, 'must enter last name'] }
   },
-  email: { type: String, required: true },
-  phone: { type: String },
-  address: {
-    number: { type: Number },
-    street: { type: String },
-    unit: { type: String },
-    city: { type: String },
-    state: { type: String },
-    zip: { type: Number }
-  },
-  pets: { type: Number },
-  note: { type: String },
-  rentRate: { type: Number },
-  property: { type: mongoose.Schema.Types.ObjectId, ref: 'Property' }
+  age: { type: Number, min: [0, 'Too young'], max: [120, 'Too old'] },
+  allergies: [ { type: String } ],
+  gender: { type: String, enum: ['male', 'female'] },
+  lastVisit: { type: Date,
+               validate: {
+                 validator: function (value) {
+                   return ((moment(value).unix()) <= (moment().unix()))
+                 },
+                 message: '{VALUE} is after today; time travelling not allowed!'
+               }
+             }
 })
 
-clientSchema.methods.updateAddress = function () {
-  this.address = this.property.address
-  return this.save()
-}
+clientSchema.statics.findCustom = function (query) {
+  let { gender, minage, maxage, visitafter, visitbefore, allergy } = query
 
-clientSchema.methods.calculateRent = function () {
-  this.rentRate = (this.property.baseRent + (this.pets * 100)) || 0
-  return this.save()
+  let fields = Object.keys(query)
+
+  let queryBuild = {}
+
+  fields.forEach(field => {
+    if (field === 'gender') queryBuild['gender'] = gender
+    if ((field === 'minage' || field === 'maxage') && !queryBuild.age) queryBuild['age'] = {}
+    if (field === 'minage') queryBuild['age']['$gt'] = minage
+    if (field === 'maxage') queryBuild['age']['$lt'] = maxage
+    // if (field === 'visitafter') queryBuild['visitafter'] = 
+  })
+
+  console.log('queryBuild: ', queryBuild)
+
+  return this.find(queryBuild)
 }
 
 const Client = mongoose.model('Client', clientSchema)
 
 module.exports = Client
-
-// GENERATOR
-
-// [
-//   '{{repeat(5, 7)}}',
-//   {
-//     name: {
-//       firstName: '{{firstName()}}',
-//       lastName: '{{surname()}}'
-//           },
-//     email: '{{email()}}',
-//     phone: '+1 {{phone()}}',
-//     address: {
-//       number: '{{integer(100, 999)}}',
-//       street: '{{street()}}',
-//       unit: '{{integer(1, 600)}}',
-//       city: '{{city()}}',
-//       state: '{{state()}}',
-//       zip: '{{integer(10000, 99999)}}'
-//              },
-//     pets: '{{integer(0, 5)}}',
-//     note: '{{lorem(1, "paragraphs")}}'
-//   }
-// ]
--- server/routes/clients.js
+++ server/routes/clients.js
@@ -3,49 +3,6 @@ const router = express.Router()
 
 const Client = require('../models/Client')
 
-// router.route('/moveIn/:id')
-//   .put((req, res) => {
-//     Client.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
-//       .then(client => {
-//         client.updateAddress()
-//         client.calculateRent()
-//         return client.save()
-//       })
-//       .then(
-//         Property.findById(req.body.property)
-//           .then(property => {
-//             if (property.tenants.indexOf(req.params.id) === -1) property.tenants.push(req.params.id)
-//             return property.save()
-//           })
-//           .catch(err => res.status(400).send(err))
-//       )
-//       .then(savedClient => res.send(savedClient))
-//       .catch(err => res.status(400).send(err))
-//   })
-//
-// router.route('/moveOut/:id')
-//   .put((req, res) => {
-//     Client.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
-//       .populate('property')
-//       .then(
-//         Property.findById(req.body.property)
-//           .then(property => {
-//             property.tenants = property.tenants.filter(id => req.params.id.toString() !== id.toString())
-//             return property.save()
-//           })
-//           .catch(err => res.status(400).send(err))
-//       )
-//       .then(client => {
-//         client.property = {
-//           _id: ''
-//         }
-//         client.calculateRent()
-//         return client.save()
-//       })
-//       .then(savedClient => res.send(savedClient))
-//       .catch(err => res.status(400).send(err))
-//   })
-
 router.route('/:id')
   .get((req, res) => {
     Client.findById(req.params.id)
@@ -60,26 +17,22 @@ router.route('/:id')
   .delete((req, res) => {
     Client.findByIdAndRemove(req.params.id)
       .then(clients => clients.save())
-      .then(
-        Client.find()
-          .then(clients => res.send(clients))
-      )
+      .then(res.send('deleted'))
       .catch(err => res.status(400).send(err))
   })
 
 router.route('/')
   .get((req, res) => {
-    Client.find()
+    Client.findCustom(req.query)
+      .skip(((parseInt(req.query.page) * (parseInt(req.query.pagesize) || 20)) - (parseInt(req.query.pagesize) || 20)) || 0)
+      .limit(parseInt(req.query.pagesize) || 20)
       .then(clients => res.send(clients))
       .catch(err => res.status(400).send(err))
   })
   .post((req, res) => {
     console.log('req.body: ', req.body)
     Client.create(req.body)
-      .then(client => {
-        // console.log('client: ', client)
-        return res.send(client)
-      })
+      .then(client => res.send(client))
       .catch(err => res.status(400).send(err))
   })
 
